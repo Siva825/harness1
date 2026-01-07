08e62cb263baf92131961c662e99b3f299b39713
Siva825 <gsp200126@gmail.com>
2026-01-07 12:31:15 +0530

    Modify Google provider project ID and instance name
    Updated project ID and changed instance resource name.

--- Iac.tf
+++ Iac.tf
@@ -1,7 +1,7 @@
 provider "google" {
-    project = "siva-477505" 
+    project = "shiva-prasad-483604" 
 }
-resource "google_compute_instance" "instance2" {
+resource "google_compute_instance" "instance1" {
     name = "vm-26"
     zone =  "us-central1-c" 
     machine_type = "e2-medium"
@@ -16,19 +16,4 @@ resource "google_compute_instance" "instance2" {
            //
         }
     }
-    metadata_startup_script = <<-EOT
-    #!/bin/bash
-
-    # Install Docker using official script (more reliable)
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sudo sh get-docker.sh
-        
-    # Start and enable Docker
-    sudo systemctl enable docker
-    sudo systemctl start docker
-        
-    # Wait for Docker to be ready
-    sleep 30
-
-    EOT
 }
